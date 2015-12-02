b237df5145c3782aa372f44d6f31ac165a5a9e09
Justin McBride <justin@dare599z.com>
2015-12-01 21:09:25 -0700

    update tracking code

--- js/analytics.js
+++ js/analytics.js
@@ -4,4 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-70835955-1', 'auto');
+ga('require', 'linkid');
 ga('send', 'pageview');
