8b1ddab3570df3bd499f1ed8390a424b54b9b71a
Justin McBride <justin@mcbride.io>
2018-01-11 16:55:12 -0700

    updates

--- js/prices.js
+++
@@ -1,8 +0,0 @@
-$.get("assets/prices.json")
-  .done(function(prices) {
-    for (var key in prices) {
-      if (prices.hasOwnProperty(key)) {
-        $("#" + key).text(prices[key]);
-      }
-    }
-  });
