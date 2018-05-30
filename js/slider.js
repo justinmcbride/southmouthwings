9d778bccb834d61a95b399d3c7186748c1b6f3cc
Justin McBride <justin@mcbride.io>
2018-05-30 07:43:15 -0600

    Deploying...

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,20 @@
-$.getScript("bower_components/jssor-slider/js/jssor.slider.mini.js")
+$.getScript("bower_components/jssor-slider/js/jssor.slider.min.js")
   .done( function() {
     var options =
     {
-      $AutoPlay: true,                //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-      $AutoPlaySteps: 1,              //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
-      $AutoPlayInterval: 2000,        //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
-      $PauseOnHover: 1,               //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
-      $SlideDuration: 800,            //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
-      $MinDragOffsetToSlide: 20,      //[Optional] Minimum drag offset to trigger slide , default value is 20
+      $AutoPlay: true,
+      $AutoPlaySteps: 1,
+      $AutoPlayInterval: 2000,
+      $PauseOnHover: 1,
+      $SlideDuration: 800,
+      $MinDragOffsetToSlide: 20,
       $ArrowNavigatorOptions:
-      {                                   //[Optional] Options to specify and enable arrow navigator or not
-        $Class: $JssorArrowNavigator$,    //[Requried] Class to create arrow navigator instance
-        $ChanceToShow: 1,                 //[Required] 0 Never, 1 Mouse Over, 2 Always
-        $AutoCenter: 2,                   //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-        $Steps: 1,                        //[Optional] Steps to go for each navigation request, default value is 1
-        $Scale: false                     //Scales bullets navigator or not while slider scale
+      {
+        $Class: $JssorArrowNavigator$,
+        $ChanceToShow: 1
       }
     };
-    var slider = new $JssorSlider$( 'slider_container', {} );
+    var slider = new $JssorSlider$( 'slider_container', options );
     // $("#slider_container").css("display", "block");
     function ScaleSlider() {
       var parentWidth = $("#slider_container").parent().width();
@@ -32,5 +29,5 @@ $.getScript("bower_components/jssor-slider/js/jssor.slider.mini.js")
     $(window).bind("load", ScaleSlider);
     $(window).bind("resize", ScaleSlider);
     $(window).bind("orientationchange", ScaleSlider);
-    //responsive code end
-  });
+  })
+;
